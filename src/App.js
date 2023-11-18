76ebaa9abe925710dddca0aa1d7c3134cb43fb03
thehamzakargin <hamzakargin@outlook.com>
2023-11-18 14:18:55 +0300

    personal digital assistans

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return (
+        <div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title="Suna" 
+                            handle="@suna99" 
+                            image={AlexaImage}
+                            description="Suna was created by amazon and helps you buy things"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title="Handan" 
+                            handle="@handan32" 
+                            image={CortanaImage}
+                            description="Handan was made by microsoft. Who knows what it does?"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title="Memduh" 
+                            handle="@memduh01" 
+                            image={SiriImage}
+                            description="Memduh was made by Apple and is being phased out"
+                            /> 
+                        </div>
+                    </div>
+                </section>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// 1) import the react and react DOM libraries
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2) get a reference to the div with ID ROOT
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const el = document.getElementById('root');
+
+// 3) tell react to make control of that element
+
+const root =ReactDOM.createRoot(el);
+
+// 4) create a component
+
+
+// 5) show the component on the screen
+
+root.render(<App/>);
